b47a068e52789aa1bcf398ffc62723d79f0987b3
Siddhartha Banerjee <banerjs.sid@gmail.com>
2019-04-19 13:35:04 -0400

    Make sure to install teleop twist keyboard

--- scripts/setup_ws.sh
+++ scripts/setup_ws.sh
@@ -28,6 +28,7 @@ sudo apt-get install -y \
         ros-melodic-robot-controllers \
         ros-melodic-slam-karto \
         ros-melodic-sound-play \
+        ros-melodic-teleop-twist-keyboard \
         ros-melodic-trac-ik \
         sox \
         vim
