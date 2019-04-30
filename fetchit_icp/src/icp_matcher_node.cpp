d83d425fa99db4d6b004c795a1311cb69ec708b0
Angel <adaruna3@gatech.edu>
2019-04-30 14:55:30 -0400

    initial working ICP between pcd files

--- fetchit_icp/src/icp_matcher_node.cpp
+++ fetchit_icp/src/icp_matcher_node.cpp
@@ -2,40 +2,81 @@
 #include <ros/package.h>
 #include <pcl/conversions.h>
 #include <pcl_conversions/pcl_conversions.h>
+#include <pcl/registration/icp.h>
 #include <pcl/io/ply_io.h>
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "icp_matcher_node");
     ros::NodeHandle matcher_nh;
-    // get file names
-    std::string mesh_file = "schuck_and_wall.ply";
-    matcher_nh.getParam("/icp_matcher_node/mesh_file", mesh_file);
+    std::string template_path = ros::package::getPath("fetchit_icp")+"/cad_models/";
 
-    std::string mesh_path = ros::package::getPath("fetchit_icp")+"/cad_models/";
-    const std::string mesh_filepath = mesh_path+mesh_file;
+    // gets template pcd file
+    std::string template_file = "schuck_and_wall.pcd";
+    matcher_nh.getParam("/icp_matcher_node/mesh_file", template_file);
+    const std::string template_filepath = template_path+template_file;
+
+    // gets environment matching pcd
+    std::string test_file = "test_mesh_easy.pcd";
+    const std::string test_filepath = template_path+test_file;
 
     // declare data structures
-    pcl::PointCloud<pcl::PointXYZ> cloud;
-    sensor_msgs::PointCloud2 cloud_msg;
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr template_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr test_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr matched_template_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
 
-    // initialize data structures
-    if (pcl::io::loadPCDFile<pcl::PointXYZ>(mesh_filepath,cloud) < 0) {
-        ROS_ERROR("Could not load PCD.");
+    // load data structures with points
+    if (pcl::io::loadPCDFile<pcl::PointXYZRGB>(template_filepath,*template_cloud) < 0) {
+        ROS_ERROR("Could not load template PCD.");
+        return -1;
+    }
+    if (pcl::io::loadPCDFile<pcl::PointXYZRGB>(test_filepath,*test_cloud) < 0) {
+        ROS_ERROR("Could not load test PCD.");
         return -1;
     }
 
-    // publish points
-    pcl::toROSMsg(cloud,cloud_msg);
-    ros::Publisher pub = matcher_nh.advertise<sensor_msgs::PointCloud2>("mesh_points",0);
-    cloud_msg.header.frame_id = "icp";
-    pub.publish(cloud_msg);
+    // prepare ICP
+    pcl::IterativeClosestPoint<pcl::PointXYZRGB,pcl::PointXYZRGB> icp;
+    icp.setInputSource(template_cloud);
+    icp.setInputTarget(test_cloud);
+
+    int iters = 50;
+    float dist = 1.0;
+    float trans = 1e-8;
+    float fit = 1;
+    matcher_nh.getParam("/icp_matcher_node/iterations",iters);
+    matcher_nh.getParam("/icp_matcher_node/max_distance",dist);
+    matcher_nh.getParam("/icp_matcher_node/trans_epsilon",trans);
+    matcher_nh.getParam("/icp_matcher_node/fit_epsilon",fit);
+
+    icp.setMaximumIterations(iters);
+    icp.setMaxCorrespondenceDistance(dist);
+    icp.setTransformationEpsilon(trans);
+    icp.setEuclideanFitnessEpsilon(fit);
+    // perform ICP
+    icp.align(*matched_template_cloud);
+
+    // prepare point clouds to publish
+    ros::Publisher pub_temp = matcher_nh.advertise<sensor_msgs::PointCloud2>("template_points",0);
+    ros::Publisher pub_test = matcher_nh.advertise<sensor_msgs::PointCloud2>("test_points",0);
+    ros::Publisher pub_mtemp = matcher_nh.advertise<sensor_msgs::PointCloud2>("matched_template_points",0);
+    sensor_msgs::PointCloud2 template_msg;
+    sensor_msgs::PointCloud2 test_msg;
+    sensor_msgs::PointCloud2 matched_template_msg;
+    pcl::toROSMsg(*template_cloud,template_msg);
+    pcl::toROSMsg(*test_cloud,test_msg);
+    pcl::toROSMsg(*matched_template_cloud,matched_template_msg);
+    template_msg.header.frame_id = "icp";
+    test_msg.header.frame_id = "icp";
+    matched_template_msg.header.frame_id = "icp";
 
     try{
         ros::Rate loop_rate(5);
         while (ros::ok())
         {
             ros::spinOnce();
-            pub.publish(cloud_msg);
+            pub_temp.publish(template_msg);
+            pub_test.publish(test_msg);
+            pub_mtemp.publish(matched_template_msg);
             loop_rate.sleep();
         }
     }catch(std::runtime_error& e){
