4de436b809e094d85997db9d44970702a1374934
Siddhartha Banerjee <banerjs.sid@gmail.com>
2019-04-30 16:06:59 -0400

    Refactor the base location monitor

--- task_execution/task_monitor/src/task_monitor/monitors/base_location_monitor.py
+++ task_execution/task_monitor/src/task_monitor/monitors/base_location_monitor.py
@@ -5,12 +5,16 @@ from __future__ import print_function, division
 
 import numpy as np
 
+from threading import Lock
+
 import rospy
-import tf
+import tf2_ros
 
-from task_execution_msgs.msg import BeliefKeys, Waypoint
-from task_execution_msgs.srv import GetWaypoints
+from task_execution_msgs.msg import BeliefKeys
+from fetchit_mapping.msg import NavigationActionGoal
 
+from tf2_geometry_msgs import do_transform_pose
+from tf.transformations import euler_from_quaternion
 from task_monitor.monitoring import AbstractBeliefMonitor
 
 
@@ -18,72 +22,45 @@ from task_monitor.monitoring import AbstractBeliefMonitor
 
 class BaseLocationMonitor(AbstractBeliefMonitor):
     """
-    Monitor the location of the base in the map and update the robot's belief of
-    the base location accordingly.
+    Monitor the location of the base in the map and update the belief of whether
+    the robot has reached its last navigation point
+
+    .. todo::
+
+        This monitor should automatically parse out the possible waypoints that
+        are defined in the different waypoints files and update the beliefs
+        for the desired waypoints automatically. The problem is programmatically
+        assigning a defined waypoint to an action in the task
     """
 
-    BASE_FRAME = "/base_link"           # Robot position
-    DESIRED_COMPARISON_FRAME = "/map"   # Reference locations will be stored in this frame
-    MONITORING_LOOP_RATE = 1            # Hz
-    LOCATION_RADIUS = 0.25              # Should be within a 25 cm radius of the location
+    BASE_FRAME = "base_link"           # Robot position
+    DESIRED_COMPARISON_FRAME = "map"   # The frame in which to compare poses
+    MONITORING_LOOP_RATE = 10           # Hz
+    LOCATION_ERROR = 0.25               # Should be within 25 cm of the goal
+    HEADING_ERROR = np.pi / 30          # Should be within 6 deg of the goal
 
-    TRANSFORM_WAIT_DURATION = rospy.Duration(10.0)
-
-    WAYPOINTS_SERVICE_NAME = "/database/waypoints"
-    PICKUP_1_LOCATION_NAMES = ["table1",]
-    DOOR_1_LOCATION_NAMES = [
-        "door1_out_facing_in",
-        "door1_out_facing_out",
-        "door1_in_facing_in",
-        "door1_in_facing_out",
-    ]
-    DROPOFF_LOCATION_NAMES = ["shelf",]
+    NAVIGATION_GOAL_TOPIC = "/navigation/goal"
 
     def __init__(self):
         super(BaseLocationMonitor, self).__init__()
 
         # Create the tf listener
-        self._listener = tf.TransformListener()
+        self._tf_buffer = tf2_ros.Buffer()
+        self._tf_listener = tf2_ros.TransformListener(self._tf_buffer)
 
-        # Connect to the database service and get the waypoint positions
-        get_waypoints_srv = rospy.ServiceProxy(BaseLocationMonitor.WAYPOINTS_SERVICE_NAME, GetWaypoints)
-        rospy.loginfo("Connecting to database services...")
-        get_waypoints_srv.wait_for_service()
-        rospy.loginfo("...database services connected")
-        belief_to_locations_map = {
-            BeliefKeys.ROBOT_AT_PICKUP_1: BaseLocationMonitor.PICKUP_1_LOCATION_NAMES,
-            BeliefKeys.ROBOT_AT_DOOR_1: BaseLocationMonitor.DOOR_1_LOCATION_NAMES,
-            BeliefKeys.ROBOT_AT_DROPOFF: BaseLocationMonitor.DROPOFF_LOCATION_NAMES,
-        }
+        # The current pose of the robot
+        self._current_location = None
 
-        # Create the dictionary of waypoint positions to check against
-        self._location_belief_checks = {
-            BeliefKeys.ROBOT_AT_PICKUP_1: [],
-            BeliefKeys.ROBOT_AT_DOOR_1: [],
-            BeliefKeys.ROBOT_AT_DROPOFF: [],
-        }
-        for location_belief in self._location_belief_checks.iterkeys():
-            for location_name in belief_to_locations_map[location_belief]:
-                # Get the waypoints and add each to the list
-                waypoints = get_waypoints_srv(location_name).waypoints
-                for waypoint in waypoints:
-                    # We do want to die on an exception, so don't catch anything
-                    # here
-                    self._listener.waitForTransform(
-                        BaseLocationMonitor.DESIRED_COMPARISON_FRAME,
-                        waypoint.frame,
-                        rospy.Time(),
-                        BaseLocationMonitor.TRANSFORM_WAIT_DURATION
-                    )
-                    (trans, _) = self._listener.lookupTransform(
-                        BaseLocationMonitor.DESIRED_COMPARISON_FRAME,
-                        waypoint.frame,
-                        rospy.Time(0)
-                    )
-                    self._location_belief_checks[location_belief].append(np.array([
-                        waypoint.x + trans[0],
-                        waypoint.y + trans[1],
-                    ]))
+        # The goal sent to the robot
+        self._last_goal = None
+        self._goal_lock = Lock()
+
+        # Setup the goal subscriber
+        self._goal_sub = rospy.Subscriber(
+            BaseLocationMonitor.NAVIGATION_GOAL_TOPIC,
+            NavigationActionGoal,
+            self._on_goal
+        )
 
         # Finally, start the monitor on a timer
         self._monitor_timer = rospy.Timer(
@@ -92,26 +69,76 @@ class BaseLocationMonitor(AbstractBeliefMonitor):
             oneshot=False
         )
 
+    def _on_goal(self, msg):
+        with self._goal_lock:
+            # Tansform the incoming goal into a pair of position and RPY in the
+            # desired comparison frame
+            try:
+                transform = self._tf_buffer.lookup_transform(
+                    BaseLocationMonitor.DESIRED_COMPARISON_FRAME,
+                    msg.header.frame_id,
+                    rospy.Time(0)
+                )
+
+                tmsg = do_transform_pose(msg, transform)
+            except (tf2_ros.LookupException, tf2_ros.ExtrapolationException):
+                return
+
+            # If the conversion was successful, then save the results
+            self._last_goal = {
+                'position': np.array([
+                    tmsg.pose.position.x,
+                    tmsg.pose.position.y,
+                    tmsg.pose.position.z,
+                ]),
+                'heading': np.array(euler_from_quaternion([
+                    tmsg.pose.orientation.x,
+                    tmsg.pose.orientation.y,
+                    tmsg.pose.orientation.z,
+                    tmsg.pose.orientation.w
+                ])),
+            }
+
     def _monitor_func(self, evt):
-        # Get the porition of the base
+        # Get the position of the base
         try:
-            (trans, _) = self._listener.lookupTransform(
+            transform = self._tf_buffer.lookup_transform(
                 BaseLocationMonitor.DESIRED_COMPARISON_FRAME,
                 BaseLocationMonitor.BASE_FRAME,
                 rospy.Time(0)
             )
-            trans = np.array(trans)
-        except (tf.LookupException, tf.ExtrapolationException):
+        except (tf2_ros.LookupException, tf2_ros.ExtrapolationException):
             return None
+
+        self._current_location = {
+            'position': np.array([
+                transform.transform.translation.x,
+                transform.transform.translation.y,
+                transform.transform.translation.z,
+            ]),
+            'heading': np.array(euler_from_quaternion([
+                transform.transform.rotation.x,
+                transform.transform.rotation.y,
+                transform.transform.rotation.z,
+                transform.transform.rotation.w,
+            ])),
+        }
+
+        # Get the rotation and translation difference
+        with self._goal_lock:
+            if self._last_goal is None:
+                return
+
+            position_error = np.linalg.norm(self._current_location['position'] - self._last_goal['position'])
+            heading_error = np.linalg.norm(self._current_location['heading'] - self._last_goal['heading'])
 
         # Then update the belief based on whether the position is within the
         # tolerance
         beliefs = {
-            belief: np.any([
-                np.linalg.norm(pos - trans[:2]) <= BaseLocationMonitor.LOCATION_RADIUS
-                for pos in positions
-            ])
-            for belief, positions in self._location_belief_checks.iteritems()
+            BeliefKeys.ROBOT_AT_NAVIGATION_GOAL: (
+                position_error <= BaseLocationMonitor.LOCATION_ERROR
+                and heading_error <= BaseLocationMonitor.HEADING_ERROR
+            )
         }
 
         # Send the updated values and return the messages that were sent
