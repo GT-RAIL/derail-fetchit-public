5d7c97892ee521b10eddfe193dbbef086f2c2e1b
Angel <adaruna3@gatech.edu>
2019-05-02 11:12:49 -0400

    updates to get cloud from robot and initial guess from launch

--- fetchit_icp/src/icp_matcher_node.cpp
+++ fetchit_icp/src/icp_matcher_node.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <ros/package.h>
+#include <ros/time.h>
 #include <pcl/conversions.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/registration/icp.h>
@@ -7,6 +8,7 @@
 #include <pcl_ros/transforms.h>
 
 #include <geometry_msgs/Transform.h>
+#include <sensor_msgs/PointCloud2.h>
 #include <tf2_ros/static_transform_broadcaster.h>
 
 int main(int argc, char** argv){
@@ -30,38 +32,70 @@ int main(int argc, char** argv){
     matcher_nh.getParam("/icp_matcher_node/template_file", template_file);
     const std::string template_filepath = template_path+template_file;
 
-    // gets environment matching pcd
-    std::string test_file = "1.pcd";
-    matcher_nh.getParam("/icp_matcher_node/test_file", test_file);
-    const std::string test_filepath = test_path+test_file;
-
     // declare data structures
+    tf::TransformListener tf_;
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr template_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr test_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr test_cloud_local(new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr test_cloud_world(new pcl::PointCloud<pcl::PointXYZRGB>);
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr matched_template_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
 
-    // load data structures with points
+    ros::Duration(5, 0).sleep();
+
+    // loads template
     if (pcl::io::loadPCDFile<pcl::PointXYZRGB>(template_filepath,*template_cloud) < 0) {
         ROS_ERROR("Could not load template PCD.");
         return -1;
     }
-    if (pcl::io::loadPCDFile<pcl::PointXYZRGB>(test_filepath,*test_cloud) < 0) {
-        ROS_ERROR("Could not load test PCD.");
+
+    // gets current point cloud
+    boost::shared_ptr<sensor_msgs::PointCloud2 const> sharedMsg;
+    sensor_msgs::PointCloud2 test_cloud_msg;
+    sharedMsg = ros::topic::waitForMessage<sensor_msgs::PointCloud2>("/head_camera/depth_registered/points");
+    if(sharedMsg != NULL){
+        test_cloud_msg = *sharedMsg;
+    } else {
+        ROS_ERROR("Could not get point cloud message from topic. Boost shared pointer is NULL.");
         return -1;
     }
+    pcl::fromROSMsg(test_cloud_msg,*test_cloud_local);
 
-    // gets the 'initial_estimate' from some other code
+    // transforms point cloud to fixed frame
+    pcl_ros::transformPointCloud("map", ros::Time(0), *test_cloud_local, test_cloud_msg.header.frame_id,
+                                 *test_cloud_world, tf_);
+
+    // gets the initial_estimate for schunk corner from the launch
+    std::string initial_estimate_string = "0 0 0 0 0 0";
+    matcher_nh.getParam("/icp_matcher_node/initial_estimate", initial_estimate_string);
+    std::vector<float> pose;
+    std::istringstream initial_estimate_string_stream(initial_estimate_string);
+    for(std::string value_string; initial_estimate_string_stream >> value_string;)
+        pose.push_back(std::stof(value_string));
+
+    // makes a tf for the initial_estimate
     tf::Transform initial_estimate;
-    initial_estimate.setOrigin(tf::Vector3(0.2,0.0,1.2));
-    initial_estimate.setRotation(tf::Quaternion(0,-0.7071068,0.7071068,0));
+    initial_estimate.setOrigin(tf::Vector3(pose[0],pose[1],pose[2]));
+    initial_estimate.setRotation(tf::Quaternion(pose[4],pose[5],pose[3]));
 
-    // transforms template by initial estimate
+    // transforms template by initial_estimate
     pcl_ros::transformPointCloud(*template_cloud,*template_cloud,initial_estimate);
+
+    // TODO DEBUG
+    ros::Publisher pub_temp = matcher_nh.advertise<sensor_msgs::PointCloud2>("template_points",0);
+    ros::Publisher pub_test = matcher_nh.advertise<sensor_msgs::PointCloud2>("test_points",0);
+    sensor_msgs::PointCloud2 template_msg;
+    sensor_msgs::PointCloud2 test_msg;
+    pcl::toROSMsg(*template_cloud,template_msg);
+    pcl::toROSMsg(*test_cloud_world,test_msg);
+    template_msg.header.frame_id = "map";
+    test_msg.header.frame_id = "map";
+    pub_temp.publish(template_msg);
+    pub_test.publish(test_msg);
+    // TODO DEBUG
 
     // prepare ICP
     pcl::IterativeClosestPoint<pcl::PointXYZRGB,pcl::PointXYZRGB> icp;
     icp.setInputSource(template_cloud);
-    icp.setInputTarget(test_cloud);
+    icp.setInputTarget(test_cloud_world);
     icp.setMaximumIterations(iters);
     icp.setMaxCorrespondenceDistance(dist);
     icp.setTransformationEpsilon(trans);
@@ -87,7 +121,7 @@ int main(int argc, char** argv){
     static tf2_ros::StaticTransformBroadcaster static_broadcaster;
     geometry_msgs::TransformStamped static_transformStamped;
     static_transformStamped.header.stamp = ros::Time::now();
-    static_transformStamped.header.frame_id = "icp";
+    static_transformStamped.header.frame_id = "map";
     static_transformStamped.child_frame_id = "guess";
     static_transformStamped.transform.translation.x = chuck_trans.x();
     static_transformStamped.transform.translation.y = chuck_trans.y();
@@ -96,28 +130,20 @@ int main(int argc, char** argv){
     static_transformStamped.transform.rotation.y = chuck_rot.y();
     static_transformStamped.transform.rotation.z = chuck_rot.z();
     static_transformStamped.transform.rotation.w = chuck_rot.w();
-    static_broadcaster.sendTransform(static_transformStamped);
     // DEBUG
 
     // prepare point clouds to publish
-    ros::Publisher pub_temp = matcher_nh.advertise<sensor_msgs::PointCloud2>("template_points",0);
-    ros::Publisher pub_test = matcher_nh.advertise<sensor_msgs::PointCloud2>("test_points",0);
     ros::Publisher pub_mtemp = matcher_nh.advertise<sensor_msgs::PointCloud2>("matched_template_points",0);
-    sensor_msgs::PointCloud2 template_msg;
-    sensor_msgs::PointCloud2 test_msg;
     sensor_msgs::PointCloud2 matched_template_msg;
-    pcl::toROSMsg(*template_cloud,template_msg);
-    pcl::toROSMsg(*test_cloud,test_msg);
     pcl::toROSMsg(*matched_template_cloud,matched_template_msg);
-    template_msg.header.frame_id = "icp";
-    test_msg.header.frame_id = "icp";
-    matched_template_msg.header.frame_id = "icp";
+    matched_template_msg.header.frame_id = "map";
 
     try{
         ros::Rate loop_rate(5);
         while (ros::ok())
         {
             ros::spinOnce();
+            static_broadcaster.sendTransform(static_transformStamped);
             pub_temp.publish(template_msg);
             pub_test.publish(test_msg);
             pub_mtemp.publish(matched_template_msg);
