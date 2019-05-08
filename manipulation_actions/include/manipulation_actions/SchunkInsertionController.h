ae1fffafa03afb968e11bc740fec44ceb6187252
Siddhartha Banerjee <banerjs.sid@gmail.com>
2019-05-08 17:01:09 -0400

    use moveit instead of KDL for kinematics

--- manipulation_actions/include/manipulation_actions/SchunkInsertionController.h
+++ manipulation_actions/include/manipulation_actions/SchunkInsertionController.h
@@ -10,13 +10,11 @@
 // boost
 #include <boost/thread/mutex.hpp>
 
-// KDL
-#include <kdl/chain.hpp>
-#include <kdl/chainjnttojacsolver.hpp>
-#include <kdl/chainfksolver.hpp>
-#include <kdl/chainfksolverpos_recursive.hpp>
-#include <kdl/frames.hpp>
-#include <kdl/tree.hpp>
+//MoveIt
+#include <moveit/robot_model_loader/robot_model_loader.h>
+#include <moveit/robot_model/robot_model.h>
+#include <moveit/robot_state/robot_state.h>
+
 
 // ROS
 #include <actionlib/client/simple_action_client.h>
@@ -44,20 +42,18 @@ public:
 
 private:
 
-    // void jointStatesCallback(const sensor_msgs::JointState &msg);
-
     void executeInsertion(const manipulation_actions::SchunkInsertGoalConstPtr &goal);
 
     // helpers
-    void setupKDL();
-    void updateJoints();
-    void updateEffort();
+    void jointStatesCallback(const sensor_msgs::JointState &msg);
+    void updateJacobian();
+    void updateJointEffort();
 
     ros::NodeHandle n, pnh;
 
     // topics
     ros::Publisher cart_twist_cmd_publisher;
-    // ros::Subscriber joint_states_subscriber;
+    ros::Subscriber joint_states_subscriber;
 
     // actionlib
     actionlib::SimpleActionServer<manipulation_actions::SchunkInsertAction> schunk_insert_server;
@@ -66,35 +62,43 @@ private:
     // TF
     tf2_ros::Buffer tf_buffer;
     tf2_ros::TransformListener tf_listener;
+    tf2::Vector3 eef_twist_goal;
+    tf2::Vector3 object_twist_goal;
 
-    // boost::mutex joint_states_mutex;
+    // *_msgs
+    sensor_msgs::JointState joint_states;
+    control_msgs::FollowJointTrajectoryGoal jnt_goal;
+    geometry_msgs::Vector3 eef_pos_;
+    geometry_msgs::Vector3 eef_pos_start;
+    geometry_msgs::Vector3 object_twist_goal_msg;
 
-    // control_msgs::FollowJointTrajectoryGoal hold_goal;
-    // sensor_msgs::JointState joint_states;
+    // std
+    std::vector<double> jnt_eff_;
+    std::vector<double> eef_force_;
+    std::vector<double> jnt_pos_;
+    std::vector<double> jnt_pos_start;
 
+    // MoveIt stuff
+    robot_model_loader::RobotModelLoader loader;
+    robot_model::RobotModelPtr kinematic_model;
+    robot_state::RobotStatePtr kinematic_state;
+    robot_state::JointModelGroup* joint_model_group;
+
+    // Eigen
+    Eigen::MatrixXd jacobian_;
+
+    // boost
+    boost::mutex joint_states_mutex;
+
+    // parameters
     double max_force;
     double insert_duration;
     double insert_tol;
     int num_trail_max;
     double reset_duration;
     double max_reset_vel;
+    int command_rate;
 
-    std::string root_link_;
-    KDL::Chain kdl_chain_;
-    boost::shared_ptr<KDL::ChainJntToJacSolver> jac_solver_;
-    KDL::JntArray jnt_pos_;
-    KDL::JntArray jnt_pos_start;
-    KDL::JntArray jnt_eff_;
-    KDL::JntArray jnt_eff_cmd;
-    control_msgs::FollowJointTrajectoryGoal jnt_goal;
-    KDL::Jacobian jacobian_;
-    std::vector<robot_controllers::JointHandlePtr> joints_;
-
-    boost::shared_ptr<KDL::ChainFkSolverPos_recursive> fksolver_;
-
-    // Robot Controllers
-    robot_controllers::ControllerManager *controller_manager_;
-    robot_controllers::CartesianTwistController twist_controller_;
 };
 
 #endif // MANIPULATION_ACTIONS_SCHUNK_INSERTION_CONTROLLER_H
--- manipulation_actions/src/SchunkInsertionController.cpp
+++ manipulation_actions/src/SchunkInsertionController.cpp
@@ -1,6 +1,7 @@
 #include <manipulation_actions/SchunkInsertionController.h>
 #include <urdf/model.h>
 #include <kdl_parser/kdl_parser.hpp>
+#include <moveit/robot_model_loader/robot_model_loader.h>
 
 using std::max;
 
@@ -9,14 +10,17 @@ SchunkInsertionController::SchunkInsertionController():
     tf_listener(tf_buffer),
     schunk_insert_server(pnh, "schunk_insert", boost::bind(&SchunkInsertionController::executeInsertion, this, _1), false),
     arm_control_client("arm_controller/follow_joint_trajectory"),
-    controller_manager_(new robot_controllers::ControllerManager)
+    loader("robot_description")
 {
-  max_force = 1; //TODO: identify the ideal threshold for detecting collision
-  insert_duration = 3; // TODO: find out the ideal duration
-  insert_tol = 0.04; //TODO: identify the ideal tolerance for detection insertion
-  max_reset_vel = 0.01; // TODO: identify the ideal maximum reset velocity
-  num_trail_max = 5; //TODO: identify the ideal num of trails
-  reset_duration = 0.5; // TODO: find out the ideal duration
+
+  // Setup parameters
+  pnh.param<int>("command_rate", command_rate, 50); // TODO: identify the ideal rate to run the controller
+  pnh.param<double>("max_force", max_force, 5); //TODO: identify the ideal threshold for detecting collision
+  pnh.param<double>("insert_duration", insert_duration, 2); // TODO: find out the ideal duration
+  pnh.param<double>("insert_tol", insert_tol, 0.04); //TODO: identify the ideal tolerance for detection insertion
+  pnh.param<double>("max_reset_vel", max_reset_vel, 0.05); // TODO: identify the ideal maximum reset velocity
+  pnh.param<int>("num_trail_max", num_trail_max, 5); //TODO: identify the ideal num of trails
+  pnh.param<double>("reset_duration", reset_duration, 0.5); // TODO: find out the ideal duration
 
   jnt_goal.trajectory.joint_names.push_back("shoulder_pan_joint");
   jnt_goal.trajectory.joint_names.push_back("shoulder_lift_joint");
@@ -27,41 +31,40 @@ SchunkInsertionController::SchunkInsertionController():
   jnt_goal.trajectory.joint_names.push_back("wrist_roll_joint");
   jnt_goal.trajectory.points.resize(1);
 
-  // jointstates subcriber to get feedback on effort
-  // joint_states_subscriber = n.subscribe("joint_states", 1, &SchunkInsertionController::jointStatesCallback, this);
+  // initialize vectors
+  eef_force_.emplace_back(0);
+  eef_force_.emplace_back(0);
+  eef_force_.emplace_back(0);
+
+  // joint_states subscriber to get feedback on effort
+  joint_states_subscriber = n.subscribe("joint_states", 1, &SchunkInsertionController::jointStatesCallback, this);
 
   // cart_twist publisher to send command to the controller
   cart_twist_cmd_publisher = n.advertise<geometry_msgs::TwistStamped>("/arm_controller/cartesian_twist/command", 1);
 
-  // Setup kinematics
-  setupKDL();
-  std::cout << "Completed KDL setup" << std::endl;
+  // Setup MoveIt
+  kinematic_model = loader.getModel();
+  robot_state::RobotStatePtr temp_kinematic_state(new robot_state::RobotState(kinematic_model));
+  joint_model_group = kinematic_model->getJointModelGroup("arm");
+  kinematic_state = temp_kinematic_state;
+  std::cout << "Completed MoveIt setup" << std::endl;
 
   // Start controller
   schunk_insert_server.start();
   std::cout << "schunk_insert_server started" << std::endl;
 }
 
-// void SchunkInsertionController::jointStatesCallback(const sensor_msgs::JointState &msg)
-// {
-//   boost::mutex::scoped_lock lock(joint_states_mutex);
-//   joint_states = msg;
-// }
+void SchunkInsertionController::jointStatesCallback(const sensor_msgs::JointState &msg)
+{
+   boost::mutex::scoped_lock lock(joint_states_mutex);
+   joint_states = msg;
+}
 
 void SchunkInsertionController::executeInsertion(const manipulation_actions::SchunkInsertGoalConstPtr& goal)
 {
-  std::cout << "Setting things up for insertion.." << std::endl;
+  std::cout << "Setting things up for insertion..." << std::endl;
+
   manipulation_actions::SchunkInsertResult result;
-
-  KDL::Vector cart_pos_;
-  KDL::Frame cart_pose_;
-  KDL::Vector eef_pos_start;
-  KDL::Frame eef_pose_start;
-
-  tf2::Vector3 eef_twist_goal;
-  std::vector<double> eef_force_{0.,0.,0.}; // initialize eef force to zero
-  geometry_msgs::Vector3 object_twist_goal_msg;
-  tf2::Vector3 object_twist_goal;
 
   // setup random seed for repeated attempts
   unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
@@ -92,77 +95,90 @@ void SchunkInsertionController::executeInsertion(const manipulation_actions::Sch
 
   cmd.twist.linear = tf2::toMsg(eef_twist_goal);
 
-  ros::Rate controller_rate(30); // TODO: find out the ideal rate
+  ros::Rate controller_rate(command_rate);
   ros::Time end_time = ros::Time::now() + ros::Duration(insert_duration);
 
   // Save initial configuration and eef position
   std::cout << "Saving initial configuration..." << std::endl;
-  updateJoints();
-  std::cout << "Initial configuration saved!" << std::endl;
 
-  jnt_pos_start = jnt_pos_;
-  if (fksolver_->JntToCart(jnt_pos_, eef_pose_start) < 0)
   {
-    ROS_ERROR_THROTTLE(1.0, "FKsolver solver failed");
+    boost::mutex::scoped_lock lock(joint_states_mutex);
+    jnt_pos_start = joint_states.position;
   }
-  eef_pos_start = eef_pose_start.p; // extract only the position
+
+  geometry_msgs::TransformStamped eef_transform_start_msg = tf_buffer.lookupTransform("base_link", "gripper_link",
+      ros::Time(0), ros::Duration(1.0));
+  eef_pos_start = eef_transform_start_msg.transform.translation; // extract only the position
+
+  std::cout << "Initial configuration saved!" << std::endl;
 
 
   // Start insertion attempts
   bool success = false;
 
-  std::cout << "Starting insertion attempts" << std::endl;
+  std::cout << "Starting insertion attempts..." << std::endl;
 
   for (unsigned int k =0 ; k < num_trail_max ; ++k)
   {
-    std::cout << "Attempt #" << k << std::endl;
+    std::cout << "Attempt #" << k+1 << std::endl;
 
-    while (ros::Time::now() < end_time || (fabs(eef_force_[0]) < max_force && fabs(eef_force_[0]) < max_force && fabs(eef_force_[2]) < max_force))
+    // Set initial eef_force to zero
+    eef_force_[0] = 0;
+    eef_force_[1] = 0;
+    eef_force_[2] = 0;
+
+    while (ros::Time::now() < end_time && (fabs(eef_force_[0]) < max_force && fabs(eef_force_[1]) < max_force && fabs
+    (eef_force_[2]) < max_force))
     {
+
       // Publish the command
       cart_twist_cmd_publisher.publish(cmd);
 
       // Compute interaction forces
-      updateJoints(); // This updates jnt_pos_
+      updateJointEffort(); // This updates jnt_eff_
 
-      updateEffort(); // This updates jnt_eff_
-
-      jac_solver_->JntToJac(jnt_pos_, jacobian_); // Get jacobian
+      updateJacobian(); // This updates jacobian_
 
       for (unsigned int i = 0 ; i < 3 ; ++i)
       {
         eef_force_[i] = 0;
-        for (unsigned int j = 0 ; j < kdl_chain_.getNrOfJoints() ; ++j)
-          eef_force_[i] += jacobian_(i,j) * jnt_eff_(j);
+        for (unsigned int j = 0 ; j < 6; ++j)
+          eef_force_[i] += jacobian_(i,j) * jnt_eff_[j];
       }
 
-      ros::spinOnce();
+      // Check for preempt
+      if (schunk_insert_server.isPreemptRequested())
+      {
+        schunk_insert_server.setPreempted(result);
+        return;
+      }
       controller_rate.sleep();
     }
 
-    // compute eef position
-    if (fksolver_->JntToCart(jnt_pos_, cart_pose_) < 0)
-    {
-      ROS_ERROR_THROTTLE(1.0, "FKsolver solver failed");
-    }
-    cart_pos_ = cart_pose_.p; // extract only the positions
-
     // Check for success
-    if (fabs(cart_pos_.y() - eef_pos_start.y()) > insert_tol)
+    std::cout << "Checking for success..." << std::endl;
+    geometry_msgs::TransformStamped eef_transform_msg_ = tf_buffer.lookupTransform("base_link", "gripper_link",
+        ros::Time(0), ros::Duration(1.0)); // update eef position
+    eef_pos_ = eef_transform_msg_.transform.translation; // extract only the position
+
+    if (fabs(eef_pos_.y - eef_pos_start.y) > insert_tol)
     {
+      std::cout << "Insertion succeeded!" << std::endl;
       success = true;
       k = num_trail_max; // end attempts if successful
     }
     else
     {
+      std::cout << "Insertion Failed!" << std::endl;
       // reset the arm to the starting point
+      std::cout << "resetting arm to original starting point..." << std::endl;
       jnt_goal.trajectory.points[0].positions.clear();
       for (size_t i = 6; i < 6 + jnt_goal.trajectory.joint_names.size(); i ++)
       {
-        jnt_goal.trajectory.points[0].positions.push_back(jnt_pos_start(i));
+        jnt_goal.trajectory.points[0].positions.push_back(jnt_pos_start[i]);
       }
       arm_control_client.sendGoal(jnt_goal);
-      arm_control_client.waitForResult(ros::Duration(0.5));
+      arm_control_client.waitForResult();
 
       // generate a random cartesian velocity to move to new position and try again
       reset_cmd.twist.linear.x = distribution(generator);
@@ -170,17 +186,26 @@ void SchunkInsertionController::executeInsertion(const manipulation_actions::Sch
       reset_cmd.twist.linear.z = distribution(generator);
 
       // apply the random velocity
+      std::cout << "Moving to a new starting point..." << std::endl;
       ros::Time reset_end_time = ros::Time::now() + ros::Duration(reset_duration);
       while (ros::Time::now() < reset_end_time)
       {
         cart_twist_cmd_publisher.publish(reset_cmd);
+
+        // Check for preempt
+        if (schunk_insert_server.isPreemptRequested())
+        {
+          schunk_insert_server.setPreempted(result);
+          return;
+        }
       }
     }
 
   }
 
   // Set success
-  if (success == true) {
+  if (success) {
+    result.success = true;
     schunk_insert_server.setSucceeded(result);
   } else {
     schunk_insert_server.setAborted(result);
@@ -188,84 +213,24 @@ void SchunkInsertionController::executeInsertion(const manipulation_actions::Sch
 
 }
 
-void SchunkInsertionController::updateJoints()
+void SchunkInsertionController::updateJacobian()
 {
-  for (size_t i = 0; i < joints_.size(); ++i)
-  {
-    std::cout << "accessing joints_ position" << std::endl;
-    joints_[i]->getPosition();
-    std::cout << "accessing jnt_pos_" << std::endl;
-    jnt_pos_(i);
-    std::cout << "accessing successful" << std::endl;
+  // Extract joint position
+  boost::mutex::scoped_lock lock(joint_states_mutex);
+  jnt_pos_ = joint_states.position;
 
-    jnt_pos_(i) = joints_[i]->getPosition();
-  }
+  // Update Jacobian
+  kinematic_state->setJointGroupPositions(joint_model_group, jnt_pos_);
+  jacobian_ = kinematic_state->getJacobian(joint_model_group);
 }
 
-void SchunkInsertionController::updateEffort()
+void SchunkInsertionController::updateJointEffort()
 {
-  for (size_t i = 0; i < joints_.size(); ++i)
-    jnt_eff_(i) = joints_[i]->getEffort();
+  boost::mutex::scoped_lock lock(joint_states_mutex);
+  jnt_eff_ = joint_states.effort;
+
 }
 
-void SchunkInsertionController::setupKDL()
-{
-  // define controller manager
-//  robot_controllers::ControllerManager manager_;
-//  robot_controllers::Controller::init(pnh, *controller_manager_);
-  controller_manager_->init(n);
-  twist_controller_.init(n, controller_manager_);
-
-  // Initialize KDL structures
-  std::string tip_link;
-  pnh.param<std::string>("root_name", root_link_, "torso_lift_link");
-  pnh.param<std::string>("tip_name", tip_link, "gripper_link");
-
-  // Load URDF
-  urdf::Model model;
-  if (!model.initParam("robot_description"))
-  {
-    ROS_ERROR("Failed to parse URDF");
-  }
-
-  // Load the tree
-  KDL::Tree kdl_tree;
-  if (!kdl_parser::treeFromUrdfModel(model, kdl_tree))
-  {
-    ROS_ERROR("Could not construct tree from URDF");
-  }
-
-  // Populate the chain
-  if(!kdl_tree.getChain(root_link_, tip_link, kdl_chain_))
-  {
-    ROS_ERROR("Could not construct chain from URDF");
-  }
-
-  jac_solver_.reset(new KDL::ChainJntToJacSolver(kdl_chain_));
-  jnt_pos_.resize(kdl_chain_.getNrOfJoints());
-  jnt_eff_.resize(kdl_chain_.getNrOfJoints());
-  jnt_eff_cmd.resize(kdl_chain_.getNrOfJoints());
-  jacobian_.resize(kdl_chain_.getNrOfJoints());
-
-  // Init joint handles
-  joints_.clear();
-  joints_.resize(kdl_chain_.getNrOfSegments());
-  std::cout << "Starting joint init" << std::endl;
-  for (size_t i = 0; i < kdl_chain_.getNrOfSegments(); ++i)
-  {
-    if (kdl_chain_.getSegment(i).getJoint().getType() != KDL::Joint::None)
-    {
-      std::cout << "loop iter: " << i << std::endl;
-      joints_.push_back(robot_controllers::JointHandlePtr(
-         controller_manager_->getJointHandle(kdl_chain_.getSegment(i).getJoint().getName())));
-      controller_manager_->getJointHandle(kdl_chain_.getSegment(i).getJoint()
-      .getName())->getPosition();
-//      joints_[i]->getPosition();
-      std::cout << "joint set" << std::endl;
-    }
-  }
-
-}
 
 int main(int argc, char **argv)
 {
