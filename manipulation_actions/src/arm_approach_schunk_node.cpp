635c084f3d59a54ef0da994d62391720afc4ad8d
Angel <adaruna3@gatech.edu>
2019-05-08 16:02:51 -0400

    Revert "better planning loop"
    This reverts commit 53dba06553519f5046e36d5886dd753ee228f529.

--- manipulation_actions/src/arm_approach_schunk_node.cpp
+++ manipulation_actions/src/arm_approach_schunk_node.cpp
@@ -33,23 +33,18 @@ int main(int argc, char **argv) {
     schunk_approach_pose.pose.orientation.z = 0;
     schunk_approach_pose.pose.orientation.w = 1;
 
-    /* TODO task will run following:
-    1. grasp large gear
-    2. move are to ready
-    3. go to origin
-    4. in hand localize
-    5. move arm to ready
-    6. go to schunk
-    7. look at schunk corner
-    8. get pose of chuck
-    9. move arm to pre-chuck approach joint pose
-    10. move arm to approach chuck pose accounting for large gear
-    Intermmedite action commands:
-    rosrun task_executor run_action.py arm '{"poses":"joint_poses.ready"}'
-    rosrun task_executor run_action.py arm '{"poses":[0.09128400231079102,-1.144515101536499,2.9994977412109374,-1.1364141782196044,-3.147721389633179,1.4671756200775146,-1.5945427632195435]}'
-    rosrun task_executor run_action.py in_hand_localize '{"disambiguate_direction":true}'
-    rosrun task_executor run_action.py load_static_octomap {}
-    */
+    // localize the object in hand
+    /*actionlib::SimpleActionClient<manipulation_actions::InHandLocalizeAction> localize_client("/in_hand_localizer/localize");
+    ROS_INFO("Waiting for in-hand localization server");
+    localize_client.waitForServer();
+    manipulation_actions::InHandLocalizeGoal ihl_goal;
+    ihl_goal.correct_object_direction = true;
+    localize_client.sendGoal(ihl_goal);
+    localize_client.waitForResult();
+    actionlib::SimpleClientGoalState status = localize_client.getState();
+    if (status == actionlib::SimpleClientGoalState::SUCCEEDED) {
+        ROS_INFO("in-hand localization succeeded");
+    }*/
 
     // removes the object from the planning scene
     bool attach_arbitrary_object = true;
@@ -81,7 +76,7 @@ int main(int argc, char **argv) {
     }
 
     // gets tf between object and gripper
-    geometry_msgs::TransformStamped object_to_gripper = tf_buffer.lookupTransform("object_frame", "wrist_roll_link",
+    geometry_msgs::TransformStamped object_to_gripper = tf_buffer.lookupTransform("object_frame", "gripper_link",
                                                                                   ros::Time(0), ros::Duration(1.0));
     tf2::Transform object_to_gripper_tf;
     tf2::fromMsg(object_to_gripper.transform, object_to_gripper_tf);
@@ -100,6 +95,15 @@ int main(int argc, char **argv) {
     geometry_msgs::PoseStamped gripper_pose_stamped;
     gripper_pose_stamped.pose = gripper_pose;
     gripper_pose_stamped.header.frame_id = "arm_approach_schunk_pose";
+
+    std::cout << gripper_pose_stamped << std::endl;
+
+    // plan and move to approach pose
+    arm_group->setPlannerId("arm[RRTConnectkConfigDefault]");
+    arm_group->setPlanningTime(1.5);
+    arm_group->setStartStateToCurrentState();
+    arm_group->setPoseTarget(gripper_pose_stamped, "gripper_link");
+    arm_group->setMaxVelocityScalingFactor(0.3);
 
     ROS_INFO("Publishing tfs");
 
@@ -122,61 +126,9 @@ int main(int argc, char **argv) {
 
     // TODO add rotations to final desired object pose
 
-    // Try planning and replanning a few times before failing
-    bool failure = false;
-    int max_planning_attempts = 3;
-    moveit::planning_interface::MoveGroupInterface::Plan grasp_plan;
-    for (int num_attempts = 0; num_attempts < max_planning_attempts; num_attempts++) {
-        ROS_INFO("Attempting to plan path to grasp. Attempt: %d/%d", num_attempts + 1, max_planning_attempts);
+    ROS_INFO("Starting to move arm");
 
-        // plan to approach pose
-        arm_group->setPlannerId("arm[RRTConnectkConfigDefault]");
-        arm_group->setPlanningTime(1.5);
-        arm_group->setStartStateToCurrentState();
-        arm_group->setPoseTarget(gripper_pose_stamped, "wrist_roll_link");
-        arm_group->setMaxVelocityScalingFactor(0.3);
-        moveit::planning_interface::MoveItErrorCode plan_result = arm_group->plan(grasp_plan);
-        std::cout << plan_result.val << std::endl;
-        // checks plan result
-        if (plan_result.val != moveit::planning_interface::MoveItErrorCode::SUCCESS
-                   && num_attempts >= max_planning_attempts - 1) {
-            ROS_INFO("Could not plan to the final grasp pose, aborting!");
-            failure = true;
-            break;
-        } else if (plan_result.val != moveit::planning_interface::MoveItErrorCode::SUCCESS) {
-            // Try again
-            ROS_INFO("Could not plan to the final grasp pose, but still more attempts. Trying again.");
-            continue;
-        }
-
-        // makes sure the plan doesn't do some roundabout RRT thing...
-        size_t check_index = static_cast<size_t>(grasp_plan.trajectory_.joint_trajectory.points.size() / 2.0);
-        double joint_error_check = 0.0;
-        double joint_error_thresh = 0.75;  // TODO: this may need tuning to be more lenient in accepting plans
-        for (size_t i = 0; i < grasp_plan.trajectory_.joint_trajectory.joint_names.size(); i++) {
-            joint_error_check += fabs(grasp_plan.trajectory_.joint_trajectory.points[0].positions[i]
-                                 - grasp_plan.trajectory_.joint_trajectory.points[check_index].positions[i]);
-        }
-
-        ROS_INFO("Joint error check on final grasp trajectory: %f", joint_error_check);
-
-        if (joint_error_check > joint_error_thresh && num_attempts >= max_planning_attempts - 1) {
-            ROS_INFO("Could not safely plan to the final grasp pose, aborting!");
-            failure = true;
-            break;
-        } else if (joint_error_check <= joint_error_thresh) {
-            // This is valid. Exit the loop!
-            break;
-        }
-    }
-
-    moveit_msgs::MoveItErrorCodes error_code;
-    error_code.val = moveit_msgs::MoveItErrorCodes::FAILURE;
-
-    if (!failure) {
-        ROS_INFO("Starting to move arm");
-        error_code = arm_group->execute(grasp_plan);
-    }
+    moveit_msgs::MoveItErrorCodes error_code = arm_group->move();
 
     if (attach_arbitrary_object) {
         arm_group->detachObject("arbitrary_gripper_object");
