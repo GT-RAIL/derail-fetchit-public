5731efe8934dc947288f07fd6514e94b627b9621
Angel <adaruna3@gatech.edu>
2019-05-09 12:39:20 -0400

    improvements to schunk approachgit status!

--- manipulation_actions/src/arm_approach_schunk_node.cpp
+++ manipulation_actions/src/arm_approach_schunk_node.cpp
@@ -12,6 +12,106 @@
 
 #include <math.h>
 
+void transformTF2ToMsg(const tf2::Transform& tf2, geometry_msgs::TransformStamped& msg, ros::Time stamp, const std::string& frame_id, const std::string& child_frame_id) {
+    msg.transform.translation.x = tf2.getOrigin().x();
+    msg.transform.translation.y = tf2.getOrigin().y();
+    msg.transform.translation.z = tf2.getOrigin().z();
+    msg.transform.rotation.x = tf2.getRotation().x();
+    msg.transform.rotation.y = tf2.getRotation().y();
+    msg.transform.rotation.z = tf2.getRotation().z();
+    msg.transform.rotation.w = tf2.getRotation().w();
+    msg.header.stamp = stamp;
+    msg.header.frame_id = frame_id;
+    msg.child_frame_id = child_frame_id;
+}
+
+geometry_msgs::TransformStamped alignObjectFrame(tf2_ros::Buffer& tf_buffer) {
+    // gets tf between object_frame and gripper
+    geometry_msgs::TransformStamped gripper_to_object = tf_buffer.lookupTransform("gripper_link", "object_frame",
+                                                                                  ros::Time(0), ros::Duration(1.0));
+    tf2::Transform gripper_to_object_tf;
+    tf2::fromMsg(gripper_to_object.transform, gripper_to_object_tf);
+
+    // extracts roll, pitch, yaw from current gripper_to_object_tf
+    tf2::Quaternion gripper_to_object_Q = gripper_to_object_tf.getRotation();
+    double roll, pitch, yaw;
+    tf2::Matrix3x3 mat(gripper_to_object_Q);
+    mat.getRPY(roll, pitch, yaw);
+
+    // sets roll amount required to align object_frame
+    double roll_amount = 0;
+    if (roll == 0) {
+        if (pitch < 0) {
+            roll_amount = -yaw;
+        } else if (pitch > 0) {
+            roll_amount = yaw+M_PI;
+        }
+    } else {
+        if (pitch < 0) {
+            roll_amount = -yaw-M_PI;
+        } else if (pitch > 0) {
+            roll_amount = yaw;
+        }
+    }
+
+    // computes the aligned_object_frame_tf
+    tf2::Quaternion update_gripper_to_object_Q = tf2::Quaternion(0,roll_amount,0);
+    tf2::Quaternion aligned_gripper_to_object_Q = gripper_to_object_Q * update_gripper_to_object_Q;
+    tf2::Transform aligned_gripper_to_object_tf;
+    aligned_gripper_to_object_tf.setOrigin(tf2::Vector3(0,0,0));
+    aligned_gripper_to_object_tf.setRotation(aligned_gripper_to_object_Q);
+
+    // generates geometry_msg from tf for publishing
+    geometry_msgs::TransformStamped aligned_gripper_to_object_transformStamped;
+    transformTF2ToMsg(aligned_gripper_to_object_tf, aligned_gripper_to_object_transformStamped,ros::Time::now(),
+                      "gripper_link","aligned_object_frame");
+    return aligned_gripper_to_object_transformStamped;
+}
+
+geometry_msgs::PoseStamped getGripperPreApproachPose(tf2_ros::Buffer& tf_buffer) {
+    // gets tf between aligned_object_frame and gripper_link
+    geometry_msgs::TransformStamped object_to_gripper = tf_buffer.lookupTransform("aligned_object_frame", "gripper_link",
+                                                                                  ros::Time(0), ros::Duration(1.0));
+    tf2::Transform object_to_gripper_tf;
+    tf2::fromMsg(object_to_gripper.transform, object_to_gripper_tf);
+
+    // gets tf between aligned_object_frame and arm_approach_schunk_pose
+    geometry_msgs::TransformStamped approach_to_object = tf_buffer.lookupTransform("arm_approach_schunk_pose", "aligned_object_frame",
+                                                                                  ros::Time(0), ros::Duration(1.0));
+
+    // gets pose to align the aligned_object_frame to the arm_approach_schunk_pose outside of the schunk
+    approach_to_object.transform.rotation.x = 0;
+    approach_to_object.transform.rotation.y = 0;
+    approach_to_object.transform.rotation.z = 0;
+    approach_to_object.transform.rotation.w = 1;
+    tf2::Transform pre_schunk_approach_pose_tf;
+    tf2::fromMsg(approach_to_object.transform, pre_schunk_approach_pose_tf);
+
+    // gets the pre_approach_gripper_pose_tf which accounts for object
+    tf2::Transform pre_approach_gripper_pose_tf;
+    pre_approach_gripper_pose_tf = pre_schunk_approach_pose_tf * object_to_gripper_tf;
+
+    // converts from tf to geometry_msgs
+    geometry_msgs::PoseStamped pre_approach_gripper_pose_stamped;
+    tf2::toMsg(pre_approach_gripper_pose_tf, pre_approach_gripper_pose_stamped.pose);
+    pre_approach_gripper_pose_stamped.header.frame_id = "arm_approach_schunk_pose";
+    return pre_approach_gripper_pose_stamped;
+}
+
+geometry_msgs::PoseStamped getGripperFinalApproachPose(tf2_ros::Buffer& tf_buffer) {
+    // gets tf between aligned_object_frame and gripper_link
+    geometry_msgs::TransformStamped object_to_gripper = tf_buffer.lookupTransform("aligned_object_frame", "gripper_link",
+                                                                                  ros::Time(0), ros::Duration(1.0));
+    tf2::Transform object_to_gripper_tf;
+    tf2::fromMsg(object_to_gripper.transform, object_to_gripper_tf);
+
+    // converts from tf to geometry_msgs
+    geometry_msgs::PoseStamped final_approach_gripper_pose_stamped;
+    tf2::toMsg(object_to_gripper_tf, final_approach_gripper_pose_stamped.pose);
+    final_approach_gripper_pose_stamped.header.frame_id = "arm_approach_schunk_pose";
+    return final_approach_gripper_pose_stamped;
+}
+
 int main(int argc, char **argv) {
     ros::init(argc, argv, "arm_approach_schunk_node");
 
@@ -24,17 +124,6 @@ int main(int argc, char **argv) {
     arm_group->startStateMonitor();
     // preps planning scene for adding collision objects as needed
     moveit::planning_interface::PlanningSceneInterface* planning_scene_interface = new moveit::planning_interface::PlanningSceneInterface();
-
-    // approach the schunk pose that is static tf
-    geometry_msgs::PoseStamped schunk_approach_pose;
-    schunk_approach_pose.header.frame_id = "arm_approach_schunk_pose";
-    schunk_approach_pose.pose.position.x = 0;
-    schunk_approach_pose.pose.position.y = 0;
-    schunk_approach_pose.pose.position.z = 0;
-    schunk_approach_pose.pose.orientation.x = 0;
-    schunk_approach_pose.pose.orientation.y = 0;
-    schunk_approach_pose.pose.orientation.z = 0;
-    schunk_approach_pose.pose.orientation.w = 1;
 
     /* TODO task will run following:
     1. grasp large gear
@@ -83,118 +172,103 @@ int main(int argc, char **argv) {
         arm_group->attachObject("arbitrary_gripper_object", "gripper_link", touch_links);
     }
 
-    // gets tf between object and gripper
-    geometry_msgs::TransformStamped gripper_to_object = tf_buffer.lookupTransform("gripper_link", "object_frame",
-                                                                                  ros::Time(0), ros::Duration(1.0));
-    tf2::Transform gripper_to_object_tf;
-    tf2::fromMsg(gripper_to_object.transform, gripper_to_object_tf);
+    // updates object_frame alignment to make chuck approach easier
+    geometry_msgs::TransformStamped aligned_object_to_gripper_tfS = alignObjectFrame(tf_buffer);
+    static tf2_ros::StaticTransformBroadcaster static_broadcaster;
+    static_broadcaster.sendTransform(aligned_object_to_gripper_tfS);
 
-    // updates alignment to make approach easier
-    tf2::Quaternion gripper_to_object_Q = gripper_to_object_tf.getRotation();
-    double roll;
-    double pitch;
-    double yaw;
-    tf2::Matrix3x3 mat(gripper_to_object_Q);
-    mat.getRPY(roll, pitch, yaw);
-
-    double roll_amount = 0;
-    if (roll == 0) {
-        if (pitch < 0) {
-            roll_amount = -yaw;
-        } else if (pitch > 0) {
-            roll_amount = yaw+M_PI;
-        }
-    } else {
-        if (pitch < 0) {
-            roll_amount = -yaw-M_PI;
-        } else if (pitch > 0) {
-            roll_amount = yaw;
-        }
-    }
-
-    tf2::Quaternion update_gripper_to_object_Q = tf2::Quaternion(0,roll_amount,0);
-    tf2::Quaternion aligned_gripper_to_object_Q = gripper_to_object_Q * update_gripper_to_object_Q;
-    tf2::Transform aligned_gripper_to_object_tf;
-    aligned_gripper_to_object_tf.setOrigin(tf2::Vector3(0,0,0));
-    aligned_gripper_to_object_tf.setRotation(aligned_gripper_to_object_Q);
-
-    geometry_msgs::Pose aligned_gripper_to_object_pose;
-    tf2::toMsg(aligned_gripper_to_object_tf, aligned_gripper_to_object_pose);
-
-    static tf2_ros::StaticTransformBroadcaster updated_object_pose_broadcaster;
-    geometry_msgs::TransformStamped aligned_gripper_to_object_transformStamped;
-    aligned_gripper_to_object_transformStamped.header.stamp = ros::Time::now();
-    aligned_gripper_to_object_transformStamped.header.frame_id = "gripper_link";
-    aligned_gripper_to_object_transformStamped.child_frame_id = "aligned_object_frame";
-    aligned_gripper_to_object_transformStamped.transform.translation.x = aligned_gripper_to_object_pose.position.x;
-    aligned_gripper_to_object_transformStamped.transform.translation.y = aligned_gripper_to_object_pose.position.y;
-    aligned_gripper_to_object_transformStamped.transform.translation.z = aligned_gripper_to_object_pose.position.z;
-    aligned_gripper_to_object_transformStamped.transform.rotation = aligned_gripper_to_object_pose.orientation;
-    updated_object_pose_broadcaster.sendTransform(aligned_gripper_to_object_transformStamped);
-
-    // gets tf between object and gripper
-    geometry_msgs::TransformStamped object_to_gripper = tf_buffer.lookupTransform("aligned_object_frame", "gripper_link",
-                                                                                  ros::Time(0), ros::Duration(1.0));
-    tf2::Transform object_to_gripper_tf;
-    tf2::fromMsg(object_to_gripper.transform, object_to_gripper_tf);
-
-    // gets tf for schunk approach pose
-    tf2::Transform schunk_approach_pose_tf;
-    tf2::fromMsg(schunk_approach_pose.pose, schunk_approach_pose_tf);
-
-    // gets the final tf which accounts for object
-    tf2::Transform gripper_pose_tf;
-    gripper_pose_tf = schunk_approach_pose_tf * object_to_gripper_tf;
-
-    // converts from tf to geometry_msgs
-    geometry_msgs::Pose gripper_pose;
-    tf2::toMsg(gripper_pose_tf, gripper_pose);
-    geometry_msgs::PoseStamped gripper_pose_stamped;
-    gripper_pose_stamped.pose = gripper_pose;
-    gripper_pose_stamped.header.frame_id = "arm_approach_schunk_pose";
+    // gets the pre_approach_gripper_pose for planning
+    geometry_msgs::PoseStamped pre_approach_gripper_pose_stamped = getGripperPreApproachPose(tf_buffer);
 
     // plan and move to approach pose
     arm_group->setPlannerId("arm[RRTConnectkConfigDefault]");
     arm_group->setPlanningTime(1.5);
     arm_group->setStartStateToCurrentState();
-    arm_group->setPoseTarget(gripper_pose_stamped, "gripper_link");
+    arm_group->setPoseTarget(pre_approach_gripper_pose_stamped, "gripper_link");
     arm_group->setMaxVelocityScalingFactor(0.3);
 
+    // debugging tfs
     ROS_INFO("Publishing tfs");
+    geometry_msgs::TransformStamped pre_approach_gripper_pose_transformStamped;
+    pre_approach_gripper_pose_transformStamped.header.stamp = ros::Time::now();
+    pre_approach_gripper_pose_transformStamped.header.frame_id = "arm_approach_schunk_pose";
+    pre_approach_gripper_pose_transformStamped.child_frame_id = "pre_approach_gripper_pose";
+    pre_approach_gripper_pose_transformStamped.transform.translation.x = pre_approach_gripper_pose_stamped.pose.position.x;
+    pre_approach_gripper_pose_transformStamped.transform.translation.y = pre_approach_gripper_pose_stamped.pose.position.y;
+    pre_approach_gripper_pose_transformStamped.transform.translation.z = pre_approach_gripper_pose_stamped.pose.position.z;
+    pre_approach_gripper_pose_transformStamped.transform.rotation = pre_approach_gripper_pose_stamped.pose.orientation;
 
-    /* TODO DEBUG
-    tf2_ros::StaticTransformBroadcaster gripper_pose_broadcaster;
-    geometry_msgs::TransformStamped gripper_pose_transformStamped;
-    gripper_pose_transformStamped.header.stamp = ros::Time::now();
-    gripper_pose_transformStamped.header.frame_id = "arm_approach_schunk_pose";
-    gripper_pose_transformStamped.child_frame_id = "final_gripper_pose";
-    gripper_pose_transformStamped.transform.translation.x = gripper_pose.position.x;
-    gripper_pose_transformStamped.transform.translation.y = gripper_pose.position.y;
-    gripper_pose_transformStamped.transform.translation.z = gripper_pose.position.z;
-    gripper_pose_transformStamped.transform.rotation = gripper_pose.orientation;
-    gripper_pose_broadcaster.sendTransform(gripper_pose_transformStamped);*/
+    // publishes transforms
+    std::vector<geometry_msgs::TransformStamped> static_poses;
+    static_poses.push_back(aligned_object_to_gripper_tfS);
+    static_poses.push_back(pre_approach_gripper_pose_transformStamped);
+    static_broadcaster.sendTransform(static_poses);
 
-    // TODO add rotations to final desired object pose
+    ros::AsyncSpinner spinner(1);
+    spinner.start();
 
-    ROS_INFO("Starting to move arm");
-    // moveit_msgs::MoveItErrorCodes error_code = arm_group->move();
-    moveit_msgs::MoveItErrorCodes error_code;
-    error_code.val = moveit_msgs::MoveItErrorCodes::FAILURE;
-
-    if (attach_arbitrary_object) {
-        arm_group->detachObject("arbitrary_gripper_object");
-        std::vector<std::string> obj_ids;
-        obj_ids.push_back("arbitrary_gripper_object");
-        planning_scene_interface->removeCollisionObjects(obj_ids);
-    }
-
+    ROS_INFO("Moving arm to pre-approach");
+    moveit_msgs::MoveItErrorCodes error_code = arm_group->move();
+    ROS_INFO("Done");
     // checks results
     if (error_code.val == moveit_msgs::MoveItErrorCodes::PREEMPTED) {
-        ROS_INFO("Preempted while moving to approach pose.");
+        ROS_INFO("Preempted while moving to pre-approach pose.");
+        if (attach_arbitrary_object) {
+            arm_group->detachObject("arbitrary_gripper_object");
+            std::vector<std::string> obj_ids;
+            obj_ids.push_back("arbitrary_gripper_object");
+            planning_scene_interface->removeCollisionObjects(obj_ids);
+        }
     } else if (error_code.val != moveit_msgs::MoveItErrorCodes::SUCCESS) {
-        ROS_INFO("Failed to move to approach pose.");
+        ROS_INFO("Failed to move to pre-approach pose.");
+        if (attach_arbitrary_object) {
+            arm_group->detachObject("arbitrary_gripper_object");
+            std::vector<std::string> obj_ids;
+            obj_ids.push_back("arbitrary_gripper_object");
+            planning_scene_interface->removeCollisionObjects(obj_ids);
+        }
     } else {
-        ROS_INFO("Succeeded to move to approach pose.");
+        ROS_INFO("Succeeded to move to pre-approach pose.");
+
+        // gets the final_approach_gripper_pose for planning
+        geometry_msgs::PoseStamped final_approach_gripper_pose_stamped = getGripperFinalApproachPose(tf_buffer);
+
+        // debugging tfs
+        ROS_INFO("Publishing tfs");
+        geometry_msgs::TransformStamped final_approach_gripper_pose_transformStamped;
+        final_approach_gripper_pose_transformStamped.header.stamp = ros::Time::now();
+        final_approach_gripper_pose_transformStamped.header.frame_id = "arm_approach_schunk_pose";
+        final_approach_gripper_pose_transformStamped.child_frame_id = "pre_approach_gripper_pose";
+        final_approach_gripper_pose_transformStamped.transform.translation.x = final_approach_gripper_pose_stamped.pose.position.x;
+        final_approach_gripper_pose_transformStamped.transform.translation.y = final_approach_gripper_pose_stamped.pose.position.y;
+        final_approach_gripper_pose_transformStamped.transform.translation.z = final_approach_gripper_pose_stamped.pose.position.z;
+        final_approach_gripper_pose_transformStamped.transform.rotation = final_approach_gripper_pose_stamped.pose.orientation;
+
+        // publishes transforms
+        static_poses.push_back(final_approach_gripper_pose_transformStamped);
+        static_broadcaster.sendTransform(static_poses);
+
+        // plans and moves to final gripper pose
+        arm_group->setStartStateToCurrentState();
+        arm_group->setPoseTarget(final_approach_gripper_pose_stamped, "gripper_link");
+        ROS_INFO("Moving arm to pre-approach");
+        error_code = arm_group->move();
+
+        if (attach_arbitrary_object) {
+            arm_group->detachObject("arbitrary_gripper_object");
+            std::vector<std::string> obj_ids;
+            obj_ids.push_back("arbitrary_gripper_object");
+            planning_scene_interface->removeCollisionObjects(obj_ids);
+        }
+
+        // checks results
+        if (error_code.val == moveit_msgs::MoveItErrorCodes::PREEMPTED) {
+            ROS_INFO("Preempted while moving to approach pose.");
+        } else if (error_code.val != moveit_msgs::MoveItErrorCodes::SUCCESS) {
+            ROS_INFO("Failed to move to approach pose.");
+        } else {
+            ROS_INFO("Succeeded to move to approach pose.");
+        }
     }
 
     try{
