65ec42c2ba935ec70511f0051fb3a8ac40ef722d
Robot Autonomy and Interactive Learning <rail@wpi.edu>
2019-05-14 14:51:41 -0400

    Addresses comments left on PR #225

--- fetchit_mapping/src/navigation_server.cpp
+++ fetchit_mapping/src/navigation_server.cpp
@@ -28,20 +28,20 @@ class NavigationActionServer
 {
 protected:
 	// Action server functionality
-    ros::NodeHandle nh_;
+    ros::NodeHandle nh_, pnh_;
     actionlib::SimpleActionServer<fetchit_mapping::NavigationAction> as_;
 
     // Odom monitor to avoid stalling/overturning issues
     const float ZERO_VEL_THRES = 0.015; // Less than this value signifies zero
-    float STALL_INITIAL_TIME = 5.0;
-    float MAX_ODOM_ROTATION = 720.0; // Rotations above this range are disallowed
     ros::Subscriber sub = nh_.subscribe("odom", 1, &NavigationActionServer::odom_callback, this);
     boost::mutex odom_mutex_; // mutex for odom callback
     nav_msgs::Odometry curr_odom_reading;
-    float prev_odom_angle_ = 180.0;
+    float max_odom_rotation; // Rotations above this range are disallowed
     float total_odom_rotation_ = 0.0;
+    float prev_odom_angle_ = 180.0;
+    float stall_initial_time; // Initial time on timer for stall detection
+    float stall_timer_;
     float prev_time_odom_ = 0.0;
-    float stall_timer_ = STALL_INITIAL_TIME;
 
     std::string action_name_;
     fetchit_mapping::NavigationFeedback feedback_;
@@ -58,17 +58,15 @@ protected:
     bool goal_reached = true;
 
     // PID control gains - angular velocity controller
-    double k_p = 1.7;
-    double k_i = 0.008;
-    double k_d = 0.0007;
+    double k_p;
+    double k_i;
+    double k_d;
 
     // we maintain constant linear velocity
     double linear_vel = 0.3;
     double angular_vel;
 
-    // stop the robot turning around - condition for abort
-    const double loop_terminate_thresh = 0.75*M_PI;
-
+    // Test parameters
     bool test_nav_param;
     string logfile_path;
 
@@ -91,21 +89,19 @@ protected:
 public:
     NavigationActionServer(std::string name) :
         as_(nh_, name, boost::bind(&NavigationActionServer::executeCB, this, _1), false),
-        action_name_(name)
+        action_name_(name),
+        pnh_("~")
     {
-        nh_.getParam("test_nav", test_nav_param);
-        nh_.getParam("logfile_path", logfile_path);
+        pnh_.getParam("test_nav", test_nav_param, false);
+        pnh_.getParam("logfile_path", logfile_path, "");
 
-        nh_.getParam("/navigation_server/k_pw", k_p);
-        nh_.getParam("/navigation_server/k_iw", k_i);
-        nh_.getParam("/navigation_server/k_dw", k_d);
+        pnh_.getParam("k_pw", k_p, 1.7);
+        pnh_.getParam("k_iw", k_i, 0.008);
+        pnh_.getParam("k_dw", k_d, 0.0007);
 
-        nh_.getParam("/navigation_server/max_odom_rotation", MAX_ODOM_ROTATION);
-        nh_.getParam("/navigation_server/stall_initial_time", STALL_INITIAL_TIME);
-
-        std::cout << "k_pw" << k_p << std::endl;
-        std::cout << "k_iw" << k_i << std::endl;
-        std::cout << "k_dw" << k_d << std::endl;
+        pnh_.getParam("max_odom_rotation", max_odom_rotation, 720.0);
+        pnh_.getParam("stall_initial_time", stall_initial_time, 15.0);
+        stall_timer_ = stall_initial_time;
 
         fetch_vel = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 1000);
         as_.start();
@@ -128,27 +124,21 @@ public:
 
         double dist_to_goal = sqrt(goal.pose.position.x*goal.pose.position.x + goal.pose.position.y*goal.pose.position.y);
 
-        //variables for PID controller 
-        double last_error= 0; 
+        //variables for PID controller
+        double last_error= 0;
         double sum_error = 0; // integration of error over time
         double prev_time = ros::Time::now().toSec();
         double delta_time;
         double error_now = atan2(goal.pose.position.y, goal.pose.position.x); // current error
-
-        double rotated_angle = 0;
-        double loop_count = 0;
 
         if(dist_to_goal > 0.1)
         {
             feedback_.status = 1;
 
             /* Rotate at the same point until heading aligns with the goal point */
-            rotated_angle = 0;
-            loop_count = 0;
-            
-            ROS_INFO("Aligning with the goal point %f", error_now*180/M_PI);
+            ROS_DEBUG("Aligning with the goal point %f", error_now*180/M_PI);
             reset_monitor_odom();
-            while(abs(error_now) > w_tolerance && abs(rotated_angle) < loop_terminate_thresh)
+            while(abs(error_now) > w_tolerance)
             {
                 if (monitor_odom()) {
                     result_.ack = -1;
@@ -158,7 +148,7 @@ public:
 
                 if(as_.isPreemptRequested() || !ros::ok())
                 {
-                    ROS_INFO("%s: Preempted", action_name_.c_str());
+                    ROS_DEBUG("%s: Preempted", action_name_.c_str());
                     as_.setPreempted();
                     success = false;
                     break;
@@ -170,14 +160,9 @@ public:
                 //PID control calculation
                 angular_vel = k_p*error_now + k_i*sum_error + k_d*(error_now-last_error)/delta_time;
 
-                vel.linear.x = 0;
-                //cout<<"velocity "<<vel.linear.x<<" "<<vel.angular.z<<endl;
-                if(loop_count != 0)
-                {
-	                rotated_angle += (angular_vel*delta_time);
-	                ROS_INFO("%f %f %f", delta_time, angular_vel, rotated_angle);	                
-                }
-                loop_count += 1;
+                vel.linear.x = 0.0;
+                vel.angular.z = angular_vel;
+                ROS_DEBUG("X Linear Velocity: %f, Z Angular Velocity: %f", vel.linear.x, vel.angular.z);
 
                 prev_time = ros::Time::now().toSec();
                 last_error = error_now;
@@ -188,33 +173,23 @@ public:
                 ros::spinOnce();
                 base_tf_listener.transformPose("base_link", ros::Time(0), world_goal, world_goal.header.frame_id, goal);
 
-                //cout<<"transformed coordinate "<<goal.pose.position.x<<" "<<goal.pose.position.y<<endl;
+                ROS_DEBUG("Transformed Coordinate: (%f, %f)", goal.pose.position.x, goal.pose.position.y);
                 error_now = atan2(goal.pose.position.y, goal.pose.position.x);
-                ROS_INFO("Velocity integral %f", rotated_angle*180/M_PI);
             }
-            // if(abs(rotated_angle) > loop_terminate_thresh)
-            // {
-            // 	result_.ack = -1;
-            // 	as_.setAborted(result_);
-            // 	return;
-            // }
 
-            ROS_INFO("Alignment complete");
+            ROS_DEBUG("Alignment complete");
 
             vel.angular.z = 0;
             fetch_vel.publish(vel);
 
-            /* move towards the goal point controlling both linear and angular velocity */
-            ROS_INFO("Moving to goal location x: %f y: %f", goal.pose.position.x, goal.pose.position.y);
+            /* Move towards the goal point controlling both linear and angular velocity */
+            ROS_DEBUG("Moving to goal location x: %f y: %f", goal.pose.position.x, goal.pose.position.y);
             sum_error = 0; // clearing the error integral
-            rotated_angle = 0;
-            loop_count = 0;
-
-            while((fabs(goal.pose.position.x) >= p_tolerance || fabs(goal.pose.position.y) >= p_tolerance) && abs(rotated_angle) < loop_terminate_thresh)
+            while(fabs(goal.pose.position.x) >= p_tolerance || fabs(goal.pose.position.y) >= p_tolerance)
             {
                 if(as_.isPreemptRequested() || !ros::ok() || (!success))
                 {
-                    ROS_INFO("%s: Preempted", action_name_.c_str());
+                    ROS_DEBUG("%s: Preempted", action_name_.c_str());
                     if(success)
                         as_.setPreempted();
                     success = false;
@@ -234,53 +209,37 @@ public:
                 last_error = error_now;
                 prev_time = ros::Time::now().toSec();
 
-                if(loop_count != 0)
-                {
-	                rotated_angle += angular_vel*delta_time;
-                }
-                loop_count += 1;
-
                 vel.linear.x = linear_vel;
                 vel.angular.z = angular_vel;
                 fetch_vel.publish(vel);
                 as_.publishFeedback(feedback_);
+                ROS_DEBUG("X Linear Velocity: %f, Z Angular Velocity: %f", vel.linear.x, vel.angular.z);
+                ROS_DEBUG("Error integral %f", sum_error*180/M_PI);
 
-                ROS_INFO("Error integral %f", sum_error*180/M_PI);
-
-                ROS_DEBUG_STREAM("velocity "<<vel.linear.x<<" "<<vel.angular.z);
                 ros::spinOnce();
                 base_tf_listener.transformPose("base_link", ros::Time(0), world_goal, world_goal.header.frame_id, goal);
                 rate.sleep();
             }
-            // if(abs(rotated_angle) > loop_terminate_thresh)
-            // {
-            // 	result_.ack = -1;
-            // 	as_.setAborted(result_);
-            // 	return;
-            // }
         }
 
-        ROS_INFO("Reached goal point");
-        vel.angular.z = 0;
-        vel.linear.x = 0;
+        ROS_DEBUG("Reached goal point");
+        vel.angular.z = 0.0;
+        vel.linear.x = 0.0;
         fetch_vel.publish(vel);
 
         // Align to the pose at the goal
-
         tf::Quaternion quat;
         tf::quaternionMsgToTF(goal.pose.orientation, quat);
         double roll, pitch;
         tf::Matrix3x3(quat).getRPY(roll, pitch, error_now);
-        ROS_INFO("Aligning to the pose angle : %f", error_now*180/M_PI);
+        ROS_DEBUG("Aligning to the pose angle : %f", error_now*180/M_PI);
         //last_error = 0; //ignore differential for first iteration
 
         if(abs(error_now) > 0.2)
         {
-        	rotated_angle = 0;
-        	loop_count = 0;
-
+            /* Rotate at the same point until heading aligns with the final goal orientation */
         	reset_monitor_odom();
-            while(abs(error_now) > w_tolerance && abs(rotated_angle) < loop_terminate_thresh)
+            while(abs(error_now) > w_tolerance)
             {
                 if (monitor_odom()) {
                     result_.ack = -1;
@@ -290,7 +249,7 @@ public:
 
                 if(as_.isPreemptRequested() || !ros::ok() || (!success))
                 {
-                    ROS_INFO("%s: Preempted", action_name_.c_str());
+                    ROS_DEBUG("%s: Preempted", action_name_.c_str());
                     if(success)
                         as_.setPreempted();
                     success = false;
@@ -301,15 +260,9 @@ public:
                 sum_error += error_now*delta_time;
                 angular_vel = k_p*error_now + k_i*sum_error + k_d*(error_now - last_error)/delta_time;
 
-                if(loop_count != 0)
-                {
-	                rotated_angle += angular_vel*delta_time;
-                }
-                loop_count += 1;
-
-                vel.linear.x = 0;
+                vel.linear.x = 0.0;
                 vel.angular.z = angular_vel;
-                //cout<<"velocity "<<vel.linear.x<<" "<<vel.angular.z<<endl;
+                ROS_DEBUG("X Linear Velocity: %f, Z Angular Velocity: %f", vel.linear.x, vel.angular.z);
 
                 prev_time = ros::Time::now().toSec();
                 last_error = error_now;
@@ -318,25 +271,18 @@ public:
                 as_.publishFeedback(feedback_);
                 rate.sleep();
                 ros::spinOnce();
+
                 goal = world_goal;
                 base_tf_listener.transformPose("base_link", ros::Time(0), world_goal, world_goal.header.frame_id, goal);
                 //cout<<"transformed coordinate "<<goal.pose.position.x<<" "<<goal.pose.position.y<<endl;
                 tf::quaternionMsgToTF(goal.pose.orientation, quat);
                 tf::Matrix3x3(quat).getRPY(roll, pitch, error_now);
-                ROS_DEBUG("current alignment error %f", error_now*180/M_PI);
+                ROS_DEBUG("Current alignment error %f", error_now*180/M_PI);
             }
-
-            feedback_.status = 5;
-            // if(abs(rotated_angle) > loop_terminate_thresh)
-            // {
-            // 	result_.ack = -1;
-            // 	as_.setAborted(result_);
-            // 	return;
-            // }
         }
         if(success)
         {
-            ROS_INFO("waypoint reached");
+            ROS_DEBUG("waypoint reached");
             result_.ack = 1;
             as_.setSucceeded(result_);
         }
@@ -375,24 +321,24 @@ public:
         ROS_DEBUG("Total rotation (Degrees): %f", total_odom_rotation_);
 
         // Over rotation check
-        if (fabs(total_odom_rotation_) > MAX_ODOM_ROTATION) {
-            ROS_INFO("ABORTED MOTION due to rotation greater than allowed range.");
+        if (fabs(total_odom_rotation_) > max_odom_rotation) {
+            ROS_DEBUG("ABORTED MOTION due to rotation greater than allowed range.");
             return true;
         }
 
-	// Stall check
+	    // Stall check
         if (fabs(odom_change) <= ZERO_VEL_THRES) {
             stall_timer_ -= ros::Time::now().toSec() - prev_time_odom_;
 
             if (stall_timer_ <= 0.0) {
-                ROS_INFO("ABORTED MOTION due to stalling for more than allowed timer.");
+                ROS_DEBUG("ABORTED MOTION due to stalling for more than allowed timer.");
                 return true;
             }
         } else {
-            stall_timer_ = STALL_INITIAL_TIME;
+            stall_timer_ = stall_initial_time;
         }
 
-        ROS_DEBUG("Stall Timer (1/3 Seconds): %f", stall_timer_);
+        ROS_DEBUG("Stall Timer (Seconds): %f", stall_timer_);
         prev_time_odom_ = ros::Time::now().toSec();
 
         return false;
@@ -407,7 +353,7 @@ public:
         tf::Matrix3x3(odom_Q).getRPY(radians_roll, radians_pitch, radians_yaw);
 
         total_odom_rotation_ = 0.0;
-        stall_timer_ = STALL_INITIAL_TIME;
+        stall_timer_ = stall_initial_time;
         prev_odom_angle_ = radians_yaw * 180.0 / M_PI + 180.0;
         prev_time_odom_ = ros::Time::now().toSec();
     }
