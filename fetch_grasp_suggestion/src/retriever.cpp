20c2a9122e44d896180bea8466ec1f45748e6ce2
David Kent <dekent90@gmail.com>
2019-05-16 14:53:01 -0400

    Grasp tuning

--- fetch_grasp_suggestion/src/retriever.cpp
+++ fetch_grasp_suggestion/src/retriever.cpp
@@ -140,7 +140,7 @@ bool Retriever::retrieveGraspsCallback(fetch_grasp_suggestion::RetrieveGrasps::R
       geometry_msgs::Pose reduced_depth_pose;
       reduced_depth_pose = adjustGraspDepth(res.grasp_list.poses[i], -0.0);
       res.grasp_list.poses[i].position = reduced_depth_pose.position;
-      res.grasp_list.poses[i].position.z -= 0.01;
+      res.grasp_list.poses[i].position.z -= 0.005;
     }
   }
 
