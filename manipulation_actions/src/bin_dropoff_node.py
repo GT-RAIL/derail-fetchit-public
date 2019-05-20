fd5427cf57fd815beba4935d163190ad095df1e3
Abhinav Jain <abhinav@Abhinavs-MacBook-Pro.local>
2019-05-20 14:04:36 -0400

    added action server to bin dropoff

--- manipulation_actions/src/bin_dropoff_node.py
+++ manipulation_actions/src/bin_dropoff_node.py
@@ -1,74 +1,85 @@
 #!/usr/bin/env python
 import rospy
+import actionlib
 import cv2
+import imutils
 from sensor_msgs.msg import Image, PointCloud2
+from manipulation_actions import BinDropoffAction, BinDropoffResult, LinearMoveAction, LinearMoveGoal
+from geometry_msgs import Point, Vector3
 
-def image_callback(raw_img):
-    gray_img = cv2.cvtColor(raw_img, cv2.COLOR_BGR2GRAY)
-    blurred_img = cv2.GaussianBlur(gray_img, (9, 9), 0)
-    thresh = cv2.threshold(blurred_img, 60, 255, cv2.THRESH_BINARY_INV)[1]
+class BinDropoff(object):
+    _result = BinDropoffResult()
 
-    # find contours in the thresholded image
-    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-                            cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
+    def __init__(self):
+        self._as = actionlib.SimpleActionServer("bin_dropoff", BinDropoffAction, execute_cb = self.image_callback, auto_start = False)
+        self.linear_client = actionlib.SimpleActionClient("linear_controller/linear_move", LinearMoveAction)
 
-    # loop over the contours
-    largest_area = 0
-    _cX = 0
-    _cY = 0
-    _c = None
-    _x = None
-    _y = None
-    _w = None
-    _h = None
-    for c in cnts:
-        peri = cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, 0.04 * peri, True)
-        if len(approx) != 4:
-            continue
-        (x, y, w, h) = cv2.boundingRect(approx)
-        print("area is:", w * h)
-        if w * h < largest_area:
-            continue
-        largest_area = w * h
 
-        # compute the center of the contour
-        M = cv2.moments(c)
-        if M["m00"] == 0:
-            continue
-        cX = int(M["m10"] / M["m00"])
-        cY = int(M["m01"] / M["m00"])
-        _cX = cX
-        _cY = cY
-        _c = c
-        _x = x
-        _y = y
-        _w = w
-        _h = h
+    def image_callback(raw_img):
+        raw_img = rospy.wait_for_message("head_camera/rgb/image_raw", Image)
+        gray_img = cv2.cvtColor(raw_img, cv2.COLOR_BGR2GRAY)
+        blurred_img = cv2.GaussianBlur(gray_img, (9, 9), 0)
+        thresh = cv2.threshold(blurred_img, 60, 255, cv2.THRESH_BINARY_INV)[1]
 
-    index = _cY * raw_img.shape[1] + _cX
+        # find contours in the thresholded image
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
 
-    pointcloud_data = rospy.wait_for_message("head_camera/depth_registered/points", PointCloud2)
-    coordinates_arr = ()
-    for i in range(-3, 3):
-        if not (pointcloud_data.data[index].x + i == 0 and pointcloud_data.data[index].y + i == 0 and pointcloud_data.data[index].z + i == 0):
-            coordinates_arr.append((pointcloud_data.data[index].x + i, pointcloud_data.data[index].y + i, pointcloud_data.data[index].z + i))
+        # loop over the contours
+        largest_area = 0
+        _cX = 0
+        _cY = 0
+        _c = None
+        _x = None
+        _y = None
+        _w = None
+        _h = None
+        for c in cnts:
+            peri = cv2.arcLength(c, True)
+            approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+            if len(approx) != 4:
+                continue
+            (x, y, w, h) = cv2.boundingRect(approx)
+            print("area is:", w * h)
+            if w * h < largest_area:
+                continue
+            largest_area = w * h
 
-    averaged_point = [sum(y) / len(y) for y in zip(*coordinates_arr)]
+            # compute the center of the contour
+            M = cv2.moments(c)
+            if M["m00"] == 0:
+                continue
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+            _cX = cX
+            _cY = cY
+            _c = c
+            _x = x
+            _y = y
+            _w = w
+            _h = h
 
+        index = _cY * raw_img.shape[1] + _cX
+
+        pointcloud_data = rospy.wait_for_message("head_camera/depth_registered/points", PointCloud2)
+        coordinates_arr = ()
+        for i in range(-3, 3):
+            if not (pointcloud_data.data[index].x + i == 0 and pointcloud_data.data[index].y + i == 0 and pointcloud_data.data[index].z + i == 0):
+                coordinates_arr.append((pointcloud_data.data[index].x + i, pointcloud_data.data[index].y + i, pointcloud_data.data[index].z + i))
+
+        averaged_point = [sum(y) / len(y) for y in zip(*coordinates_arr)]
+
+        linear_goal = LinearMoveGoal()
+        linear_goal.point.x = averaged_point[0]
+        linear_goal.point.y = averaged_point[1]
+        linear_goal.point.z = averaged_point[2]
+        linear_goal.hold_final_pose = True
+        self.linear_client.send_goal(linear_goal)
+        self.linear_client.wait_for_result()
     
 
-
-def bin_dropoff_node():
-    """
-    Takes in an image coordinate and determines the world coordinate.
-    """
-
-    rospy.init_node('listener')
-
-    rospy.Subscriber('head_camera/rgb/image_raw', Image, image_callback)
-    rospy.spin()
-
 if __name__ == "__main__":
-    bin_dropoff_node()
+    rospy.init_node("bin_dropoff")
+    bin_dropoff = BinDropoff()
+    rospy.spin()
