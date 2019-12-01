291dca3a7ff6337f458230bbac1037acc8957dff
Weiyu Liu <wliu88@gatech.edu>
2019-12-01 17:53:10 -0500

    Added an option to segment objects without affordance detection

--- rail_semantic_grasping/include/rail_semantic_grasping/object_semantic_segmentation.h
+++ rail_semantic_grasping/include/rail_semantic_grasping/object_semantic_segmentation.h
@@ -62,11 +62,6 @@
 #include <pcl/features/normal_3d.h>
 
 
-
-
-// YAML
-//#include <yaml-cpp/yaml.h>
-
 // BOOST
 #include <boost/thread/mutex.hpp>
 
@@ -189,30 +184,6 @@ private:
  */
   void depthImageCallback(const sensor_msgs::ImageConstPtr &depth_img);
 
-//  /*!
-//   * \brief Determine the current zone based on the latest state of the TF tree.
-//   *
-//   * Checks each segmenation zone criteria based on teh latest state of the TF tree and returns a reference to that
-//   * zone. If multiple zones are met, the first is returned. If no valid zone is found, the first zone is returned
-//   * and a warning is sent to ROS_WARN.
-//   *
-//   * \return The zone that matches the current state of the TF tree.
-//   */
-//  const SegmentationZone &getCurrentZone() const;
-//
-//  /*!
-//   * \brief Callback for the remove object request.
-//   *
-//   * Remote the object from the segmented object list with a given ID. This will publish both an updated segmented
-//   * object list and a marker array with a delete action for the given marker index.
-//   *
-//   * \param req The request with the index to use.
-//   * \param res The empty response (unused).
-//   * \return Returns true if a valid index was provided.
-//   */
-//  bool removeObjectCallback(rail_segmentation::RemoveObject::Request &req,
-//      rail_segmentation::RemoveObject::Response &res);
-//
   /*!
    * \brief Callback for the clear request.
    *
@@ -237,21 +208,29 @@ private:
    */
   bool segmentCallback(std_srvs::Empty::Request &req, std_srvs::Empty::Response &res);
 
-//  bool calculateFeaturesCallback(rail_manipulation_msgs::ProcessSegmentedObjects::Request &req,
-//      rail_manipulation_msgs::ProcessSegmentedObjects::Response &res);
 
-//  /*!
-//   * \brief Callback for the main segmentation request.
-//   *
-//   * Performs a segmenation with the latest point cloud. This will publish both a segmented object list and a marker
-//   * array of the resulting segmentation.
-//   *
-//   * \param req The empty request (unused).
-//   * \param res The resulting segmented object list.
-//   * \return Returns true if the segmentation was successful.
-//   */
+  /*!
+   * \brief Callback for the main segmentation request.
+   *
+   * Performs a segmenation with the latest point cloud. This will publish both a segmented object list and a marker
+   * array of the resulting segmentation.
+   *
+   * \param req The empty request (unused).
+   * \param res The resulting segmented object list.
+   * \return Returns true if the segmentation was successful.
+   */
   bool segmentObjectsCallback(rail_semantic_grasping::SegmentSemanticObjectsRequest &req,
                               rail_semantic_grasping::SegmentSemanticObjectsResponse &res);
+
+  /*!
+   * \brief Callback for the semantic segmentation request.
+   *
+   * Performs a semantic segmenation with the latest point cloud. This will publish both a segmented object list and a marker
+   * array of the resulting segmentation.
+   *
+   * \param objects List for resulting segmented objects.
+   */
+  bool segmentObjects(rail_semantic_grasping::SemanticObjectList &objects);
 
   /*!
    * \brief Callback for the semantic segmentation request.
@@ -261,7 +240,7 @@ private:
    *
    * \param objects List for resulting segmented objects.
    */
-  bool segmentObjects(rail_semantic_grasping::SemanticObjectList &objects);
+  bool segmentObjectsWithoutAffordance(rail_semantic_grasping::SemanticObjectList &objects);
 
   /*!
    * \brief Callback for the geometric segmentation request.
@@ -289,85 +268,6 @@ private:
                       pcl::PointCloud<pcl::PointXYZRGB>::Ptr &body,
                       pcl::PointCloud<pcl::PointXYZRGB>::Ptr &opening);
 
-//  /*!
-//   * \brief Find and remove a surface from the given point cloud.
-//   *
-//   * Find a surface in the input point cloud and attempt to remove it. The surface must be within the bounds provided
-//   * in order to be removed. The resulting point cloud is placed in the output cloud. If no surface is found, no
-//   * effect is made to the output cloud and negative infinity is returned.
-//   *
-//   * \param in The input point cloud.
-//   * \param indices_in The indices in the point cloud to consider.
-//   * \param z_min The minimum height of a surface to remove.
-//   * \param z_max The maximum height of a surface to remove.
-//   * \param indices_out The set of points that are not part of the surface.
-//   * \param table_out the table as a segmented object, to be published on a separate topic
-//   * \return true if a surface was found, false otherwise
-//   */
-//  bool findSurface(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr &in, const pcl::IndicesConstPtr &indices_in,
-//      const SegmentationZone &zone, const pcl::IndicesPtr &indices_out,
-//      rail_manipulation_msgs::SegmentedObject &table_out) const;
-//
-//  /*!
-//   * \brief Find clusters in a point cloud.
-//   *
-//   * Find the clusters in the given point cloud using euclidean cluster extraction and a KD search tree.
-//   *
-//   * \param in The point cloud to search for point clouds from.
-//   * \param indices_in The indices in the point cloud to consider.
-//   * \param clusters The indices of each cluster in the point cloud.
-//   */
-//  void extractClustersEuclidean(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr &in, const pcl::IndicesConstPtr &indices_in,
-//      std::vector<pcl::PointIndices> &clusters) const;
-//
-//
-//  /*!
-//   * \brief Find clusters in a point cloud.
-//   *
-//   * Find the clusters in the given point cloud using RGB region growing and a KD search tree.
-//   *
-//   * \param in The point cloud to search for point clouds from.
-//   * \param indices_in The indices in the point cloud to consider.
-//   * \param clusters The indices of each cluster in the point cloud.
-//   */
-//  void extractClustersRGB(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr &in, const pcl::IndicesConstPtr &indices_in,
-//                          std::vector<pcl::PointIndices> &clusters) const;
-//
-//  /*!
-//   * \brief Bound a point cloud based on the inverse of a set of conditions.
-//   *
-//   * Extract a new point cloud based on the inverse of a set of conditions.
-//   *
-//   * \param in The point cloud to take points from.
-//   * \param indices_in The indices in the point cloud to consider.
-//   * \param conditions The conditions specifying which points to ignore.
-//   * \param indices_out The set of points that pass the condition test.
-//   */
-//  void inverseBound(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr &in, const pcl::IndicesConstPtr &indices_in,
-//      const pcl::ConditionBase<pcl::PointXYZRGB>::Ptr &conditions, const pcl::IndicesPtr &indices_out) const;
-//
-//  /*!
-//   * \brief Extract a new point cloud based on the given indices.
-//   *
-//   * Extract a new point cloud from the given indices. The resulting point cloud will be unorganized.
-//   *
-//   * \param in The point cloud to take points from.
-//   * \param indices_in The indices to create a new point cloud from.
-//   * \param out The point cloud to fill.
-//   */
-//  void extract(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr &in, const pcl::IndicesConstPtr &indices_in,
-//      const pcl::PointCloud<pcl::PointXYZRGB>::Ptr &out) const;
-//
-//  /*!
-//   * \brief Find the average Z value of the point vector.
-//   *
-//   * Finds the average Z value of the point vector. An empty vector will return an average of 0.
-//   *
-//   * \param v The vector of points to average.
-//   * \return The average Z value of the provided points.
-//   */
-//  double averageZ(const std::vector<pcl::PointXYZRGB, Eigen::aligned_allocator<pcl::PointXYZRGB> > &v) const;
-//
   /*!
    * \brief Create a Marker from the given point cloud.
    *
@@ -381,19 +281,8 @@ private:
 
   visualization_msgs::Marker createTextMarker(const std::string &label, const std_msgs::Header &header,
       const geometry_msgs::Point &position, const std::string &marker_namespace) const;
-//
-//  /*!
-//   * \brief Create a cropped image of the segmented object.
-//   *
-//   * Creates a new ROS image based on the cropped segmented object.
-//   *
-//   * \param in The original organized point cloud.
-//   * \param cluster The indicies of the current cluster in the point cloud.
-//   * \return The corresponding image for the given cluster.
-//   */
-//  sensor_msgs::Image createImage(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr &in,
-//      const pcl::PointIndices &cluster) const;
 
+  bool use_affordance_segmentation_;
   std::string point_cloud_topic_, camera_color_topic_, camera_depth_topic_;
   /*! The debug, okay check, first point cloud, and color segmentation flags. */
   bool debug_, okay_, first_pc_in_, use_color_, first_color_in_, first_depth_in_;
@@ -458,7 +347,5 @@ private:
 
 }
 }
-
-//Eigen::Vector3f RGB2Lab (const Eigen::Vector3f& colorRGB);
 
 #endif
